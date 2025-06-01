8692f5a77367f18b6c0691016c2b1069a68d8437
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2025-06-01 02:29:12 -0300

    refactor: improve wallet creation logic and enhance error handling

--- app/api/wallet/create.ts
+++ app/api/wallet/create.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 
 const saveWallet = async (userId: string, data: { mnemonic: string; address: string; publicKey: string }) => {
   try {
-    await prisma.wallet.create({
+    const newWallet = await prisma.wallet.create({
       data: {
         userId,
         mnemonic: data.mnemonic,
@@ -15,14 +15,46 @@ const saveWallet = async (userId: string, data: { mnemonic: string; address: str
         publicKey: data.publicKey,
       },
     });
+    return {
+      address: newWallet.address,
+      mnemonic: newWallet.mnemonic,
+      new: true,
+    };
+
   } catch (error) {
     console.error(`Error saving wallet for user ${userId}:`, error);
     throw new Error('Failed to save wallet');
   }
 };
 
+const findWallet = async (userId: string) => {
+  const existingWallet = await prisma.wallet.findUnique({
+      where: {
+        userId,
+      },
+    });
+    if(existingWallet) {
+      console.log(`Wallet already exists for user ${userId}, skipping creation.`);
+      return {
+        address: existingWallet.address,
+        mnemonic: existingWallet.mnemonic,
+        new: false,
+      }
+    }
+    return null;
+};
+
 export const createWalletForUser = async (userId: string) => {
   await cryptoWaitReady(); // inicializa crypto WASM
+
+  const existingWallet = await findWallet(userId);
+  if (existingWallet) {
+    const keyring = new Keyring({ type: 'sr25519' });
+    const oldPair = keyring.addFromUri(existingWallet.mnemonic);
+    const address = encodeAddress(oldPair.publicKey, 42);
+    existingWallet.address = address;
+    return existingWallet;
+  }
 
   const mnemonic = mnemonicGenerate();
 
@@ -30,16 +62,19 @@ export const createWalletForUser = async (userId: string) => {
   const keyring = new Keyring({ type: 'sr25519' });
   const pair = keyring.addFromUri(mnemonic);
 
-  const address = encodeAddress(pair.publicKey, 0); // 0 is the prefix for Polkadot/Substrate
+  const address = encodeAddress(pair.publicKey, 42); // 0 is the prefix for Polkadot/Substrate, 42 is for testnet
 
-  saveWallet(userId, {
+  const savedWallet = await saveWallet(userId, {
     mnemonic,
     address,
     publicKey: u8aToHex(pair.publicKey),
   });
 
-  console.log(`✅ Wallet creada para ${userId}`);
-  console.log(`📬 Dirección: ${address}`);
+  if (!savedWallet) {
+    throw new Error('Failed to create wallet');
+  }
 
-  return { address, mnemonic };
+  return savedWallet;
 };
+
+
