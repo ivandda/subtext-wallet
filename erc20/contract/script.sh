ca862506e704bcf22332cb793bb5eeeacbf716ba
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2025-06-01 10:29:45 -0300

    FEAT: Add initial ERC-20 contract implementation with metadata and WASM binary
    - Created erc20.json containing contract metadata including constructors, events, and messages.
    - Added erc20.wasm as the compiled WebAssembly binary for the ERC-20 contract.

---
+++ erc20/contract/script.sh
@@ -0,0 +1,5 @@
+pop up contract \
+  --constructor new \
+  --args "SubText Wallet Token" "SWT" 10 1000000000 \
+  --suri "large saddle net rubber minute rigid embrace matter field net wave keen" \
+  --url wss://rpc1.paseo.popnetwork.xyz
